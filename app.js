dbc22d5944f5944bd81559ce88b9adcaa52302c7
Marcel Vitor Junqui <163474051+VJunqui@users.noreply.github.com>
2025-01-23 17:44:31 -0300

    deploy jogo js

---
+++ app.js
@@ -0,0 +1,27 @@
+alert('Boas vindas ao jogo do número secreto!');
+let numeroSecreto = 4;
+let chute
+
+// Valor do chute no log
+console.log('Valor do chute', chute)
+
+// Valor do número secreto no log
+console.log('Valor do número secreto', numeroSecreto);
+
+// Resultado da expressão no log
+console.log('Resultado da comparação:', chute == numeroSecreto);
+
+// Enquanto o chute não for igual
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 30');
+    if (chute == numeroSecreto) {
+        alert('Isso aí, você acertou o número secreto: '+numeroSecreto)
+    }   else {
+        if (chute < numeroSecreto) {
+            alert('O número secreto é maior do que ' +chute);
+        } else {
+            alert('O número secreto e menor do que ' +chute);
+        }
+    };
+        
+}
